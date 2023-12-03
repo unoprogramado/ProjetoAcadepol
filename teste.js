686e255f536af98ee2753a0c7c48ef13cc990311
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-02 22:43:40 -0300

    testando o teste.js

--- teste.js
+++ teste.js
@@ -36,87 +36,87 @@ app.use(express.json())
 
 var tipo 
 
-app.get("/login",function(req, res){
+// app.get("/login",function(req, res){
     
-      res.render('login')
+//       res.render('login')
    
-   })
+//    })
 
-app.post("/login",function(req, res,next){
+// app.post("/login",function(req, res,next){
 
     
-    passport.authenticate("local",{
-        successRedirect: "/",
-        failureRedirect: "/login",
+//     passport.authenticate("local",{
+//         successRedirect: "/",
+//         failureRedirect: "/login",
         
         
-    })(req,res,next)
+//     })(req,res,next)
     
-})
-app.get("/",isAuthenticated,function(req, res){
+// })
+app.get("/",function(req, res){
     console.log(req.user.Nome)
     res.render("home")
 
 })
 
-app.get("/atribuicao",isAuthenticated,function(req, res){
+// app.get("/atribuicao",isAuthenticated,function(req, res){
     
- tipo = "atribuicao"
-   res.render('atribuicao', {user: req.user.Nome})
+//  tipo = "atribuicao"
+//    res.render('atribuicao', {user: req.user.Nome})
    
-})
+// })
   
-app.get("/desligamento",isAuthenticated,function(req, res){
-    tipo = "Desligamento"
-    res.render('desligamento', {user: req.user.Nome})
+// app.get("/desligamento",isAuthenticated,function(req, res){
+//     tipo = "Desligamento"
+//     res.render('desligamento', {user: req.user.Nome})
  
- })
+//  })
 
- app.get("/promocao",isAuthenticated,function(req, res){
-    tipo = "Promocao"
-    res.render('promocao',{user: req.user.Nome})
+//  app.get("/promocao",isAuthenticated,function(req, res){
+//     tipo = "Promocao"
+//     res.render('promocao',{user: req.user.Nome})
  
- })
+//  })
  
-app.post("/pdf",isAuthenticated,function(req, res){
+// app.post("/pdf",isAuthenticated,function(req, res){
     
    
        
-        var dados = req.body
+//         var dados = req.body
         
-        hbs.handlebars.registerHelper("if", function(a) {
-            if(a == "atribuicao" ){
-             return `Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong>da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do RG de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de minhas atribuições, decido por: Atribuir o Sr.(a) <strong>${dados.NomeDoPromovido.toUpperCase()}</strong> portador do RG de Nº <strong>${dados.RGDoPromovido.toUpperCase()}</strong> atual <strong>${dados.CargoDPD.toUpperCase()}</strong> do Departamento de Polícia do Destiny, para funções de <strong>${dados.NovaFunção.toUpperCase()}</strong> da Academia De Policia, com atribuições de auxiliar e promover as necessidades do setor.`
+//         hbs.handlebars.registerHelper("if", function(a) {
+//             if(a == "atribuicao" ){
+//              return `Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong>da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do RG de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de minhas atribuições, decido por: Atribuir o Sr.(a) <strong>${dados.NomeDoPromovido.toUpperCase()}</strong> portador do RG de Nº <strong>${dados.RGDoPromovido.toUpperCase()}</strong> atual <strong>${dados.CargoDPD.toUpperCase()}</strong> do Departamento de Polícia do Destiny, para funções de <strong>${dados.NovaFunção.toUpperCase()}</strong> da Academia De Policia, com atribuições de auxiliar e promover as necessidades do setor.`
             
-            }else if(a == "Desligamento"){
-                return ` Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong> da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do <strong>RG</strong> de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de minhas atribuições, decido por: Desligar de suas funções exercidas na academia de polícia o atual <strong>${dados.CargoPunido.toUpperCase()}</strong>  <strong>${dados.NomeDoPunido.toUpperCase()}</strong> portador do <strong>RG</strong> de Nº <strong>${dados.RGDoPunido.toUpperCase()}</strong> das funções  <strong>${dados.FuncaoDoPunido.toUpperCase()}</strong> Agradecemos seus serviços em prol da academia.`
-            }
-            else if(a == "Promocao"){
-                return `Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong> da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do RG de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de 
+//             }else if(a == "Desligamento"){
+//                 return ` Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong> da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do <strong>RG</strong> de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de minhas atribuições, decido por: Desligar de suas funções exercidas na academia de polícia o atual <strong>${dados.CargoPunido.toUpperCase()}</strong>  <strong>${dados.NomeDoPunido.toUpperCase()}</strong> portador do <strong>RG</strong> de Nº <strong>${dados.RGDoPunido.toUpperCase()}</strong> das funções  <strong>${dados.FuncaoDoPunido.toUpperCase()}</strong> Agradecemos seus serviços em prol da academia.`
+//             }
+//             else if(a == "Promocao"){
+//                 return `Eu, <strong>${dados.FuncaoResponsavel.toUpperCase()}</strong> da academia de polícia <strong>${dados.NomeResponsavel.toUpperCase()} </strong> portador(a) do RG de Nº <strong>${dados.RgResponsavel.toUpperCase()}</strong> no uso de 
 
-                minhas atribuições, decido por: Promover o Sr.(a) <strong>${dados.NomeDoPromovido.toUpperCase()}</strong> portador do RG de Nº <strong>${dados.RGDoPromovido.toUpperCase()}</strong>  atual <strong>${dados.AtualCargo.toUpperCase()}</strong> , para funções de <strong>${dados.NovoCargo.toUpperCase()}</strong> , e responsável por <strong>${dados.FuncaoDoPromovido.toUpperCase()}</strong> durante o período na ACADEPOL em consonância com a decisão conjunta da direção acadepol.`
-            }
+//                 minhas atribuições, decido por: Promover o Sr.(a) <strong>${dados.NomeDoPromovido.toUpperCase()}</strong> portador do RG de Nº <strong>${dados.RGDoPromovido.toUpperCase()}</strong>  atual <strong>${dados.AtualCargo.toUpperCase()}</strong> , para funções de <strong>${dados.NovoCargo.toUpperCase()}</strong> , e responsável por <strong>${dados.FuncaoDoPromovido.toUpperCase()}</strong> durante o período na ACADEPOL em consonância com a decisão conjunta da direção acadepol.`
+//             }
              
-           });
+//            });
   
-        res.render('pdf',{assinatura:req.user.Nome,tipo:tipo, data:Data
+//         res.render('pdf',{assinatura:req.user.Nome,tipo:tipo, data:Data
         
-        })
+//         })
    
     
-    // console.log(typeof(req.body.lucas))
+//     // console.log(typeof(req.body.lucas))
    
- })
+//  })
 
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('/login');
-  }
+// function isAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return next();
+//     }
+//     res.redirect('/login');
+//   }
 
-app.listen(8081, function(){
-    console.log("Servidor rondando ")
-})
+// app.listen(8081, function(){
+//     console.log("Servidor rondando ")
+// })
 
 
