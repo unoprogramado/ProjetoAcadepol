fadd22e6e933196eb73255c7d22cc7a014effccc
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-03 21:49:51 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -11,7 +11,7 @@ const Sequelize = require("sequelize")
 // const passport = require("passport")
 // require("./config/auth")(passport)
 
-
+console.log(process.env.DB_HOST)
 const sequelize =  new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
