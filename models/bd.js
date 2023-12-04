0d0edc8b568054db87b9d87e8eee288b601b486e
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-03 22:52:41 -0300

    Update bd.js

--- models/bd.js
+++ models/bd.js
@@ -3,7 +3,8 @@ const { Sequelize } = require("sequelize")
 const sequelize =  new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
-    dialect: process.env.BD
+    dialect: process.env.BD,
+    dialectModule: require('mysql2'),
 })
 
 
