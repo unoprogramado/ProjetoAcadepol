d7203d3e72b38e0e71fad20eafeba89b5324d1fe
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-03 21:37:25 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -13,11 +13,7 @@ const Sequelize = require("sequelize")
 
 
 
-const sequelize =  new Sequelize ("railway", "root", "-Bg4Fa6HDGe-cG544Df13F25G4d2aCA4", {
-    host: "viaduct.proxy.rlwy.net",
-    port:"22445",
-    dialect: "mysql"
-})
+
 
 
 
