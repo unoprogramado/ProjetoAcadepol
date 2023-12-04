28a2376011ee7151f2c4b784cb05b3068d31f23f
Lucas Bispo <113217544+unoprogramado@users.noreply.github.com>
2023-12-03 23:16:31 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -50,7 +50,7 @@ app.use(express.static(path.join(__dirname,'/views/public')))
 // app.use(bodyParser.json())
   
 
-usuarioDb.findOne({where:{nome: "Lucas Bispo"}}).then((usuario) => {console.log(usuario)})
+usuarioDb.findOne({where:{nome: "Lucas Bispo"}}).then((usuario) => {console.log("bispo"+usuario)})
 
 // var tipo 
 
